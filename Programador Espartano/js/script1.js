0e63b65310d27696653e5bd7924a9bac7678925c
Biellms <biell.mendes8@gmail.com>
2022-09-08 17:22:27 -0300

    Add espartano js

---
+++ Programador Espartano/js/script1.js
@@ -0,0 +1,22 @@
+class Produto {
+
+    constructor() {
+        this.id = 0;
+        this.nomeProduto = '';
+        this.valor = 0;
+    }
+
+    adicionar() {
+        alert('Item adicionado!')
+    }
+
+    deletar() {
+        alert('Item deletado!')
+    }
+
+    editar() {
+        alert('Item editado!')
+    }
+}
+
+var produto = new Produto;
