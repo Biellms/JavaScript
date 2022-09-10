6581fa07043b4e0de069bfba8a8c8b3077f1607c
Biellms <biell.mendes8@gmail.com>
2022-09-10 02:00:15 -0300

    Add and Upd script1

--- Programador Espartano/js/script1.js
+++ Programador Espartano/js/script1.js
@@ -1,22 +1,85 @@
 class Produto {
 
     constructor() {
-        this.id = 0;
-        this.nomeProduto = '';
-        this.valor = 0;
+        this.id = 1;
+        this.arrayProdutos = [];
     }
 
-    adicionar() {
-        alert('Item adicionado!')
+    salvar() {
+        let produto = this.lerDados();
+
+        if (this.validaCampos(produto)) {
+            this.adicionar(produto);
+        }
+        
+        this.listaTabela();
+        this.cancelar();
     }
 
-    deletar() {
-        alert('Item deletado!')
+    listaTabela() {
+        let tbody = document.getElementById('tbody');
+        tbody.innerText = '';
+
+        for (let i = 0; i < this.arrayProdutos.length; i++) { 
+            let tr = tbody.insertRow();
+
+            let td_id = tr.insertCell();
+            let td_nomeProduto = tr.insertCell();
+            let td_preco = tr.insertCell();
+            let td_acoes = tr.insertCell();
+            let imgEdit = document.createElement('img');
+            let imgDelete = document.createElement('img');
+            
+            imgEdit.src = './img/edit.png';
+            imgDelete.src = './img/delete.png';
+
+            td_id.innerText = this.arrayProdutos[i].id;
+            td_nomeProduto.innerText = this.arrayProdutos[i].nomeProduto;
+            td_preco.innerText = 'R$ '+this.arrayProdutos[i].preco;
+            td_acoes.appendChild(imgEdit);
+            td_acoes.appendChild(imgDelete);
+        }
     }
 
-    editar() {
-        alert('Item editado!')
+    adicionar(produto) {
+        this.arrayProdutos.push(produto);
+        this.id++;
     }
+
+    lerDados() {
+        let produto = {}
+
+        produto.id = this.id
+        produto.nomeProduto = document.getElementById('produto').value;
+        produto.preco = document.getElementById('preco').value;
+
+        return produto;
+    }
+
+    validaCampos(produto) {
+        let msg = '';
+
+        if(produto.nomeProduto == '') {
+           msg += 'Informe o nome do produto\n'
+        }
+
+        if(produto.preco == '') {
+            msg += 'Informe o preço do produto' 
+        }
+
+        if (msg != '') {
+            alert(msg)
+            return false;
+        }
+
+        return true;
+    }
+
+    cancelar() {
+        document.getElementById('produto').value = '';
+        document.getElementById('preco').value = '';
+    }
+
 }
 
 var produto = new Produto;
